514be0f43e8ad605c374eec36787b8ce252115f1
Dan Pham <phamdan.sj@gmail.com>
2024-10-19 14:31:42 -0700

    Implement Pokecard

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Pokecard from "./Pokecard";
 
 function App() {
 	return (
 		<div className="App">
 			<header className="Pokedex">
 				<h1>Welcome to the Pokedex!</h1>
+				<Pokecard />
 			</header>
 		</div>
 	);
