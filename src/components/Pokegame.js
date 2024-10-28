b5196657d3c7bcb2f149d604d96bfb8f58a73025
Dan Pham <phamdan.sj@gmail.com>
2024-10-28 15:20:10 -0700

    Calculate total experience of each hand and display on screen

--- src/components/Pokegame.js
+++ src/components/Pokegame.js
@@ -8,12 +8,30 @@ const Pokegame = ({ pokemon = pokemonData }) => {
 	const shuffledPokemon = shuffle(pokemon);
 	const [hand1, hand2] = [shuffledPokemon.slice(0, 4), shuffledPokemon.slice(4)];
 
+	const sumTotalExperience = (hand) =>
+		hand.reduce((total, { base_experience }) => total + base_experience, 0);
+
+	const experienceHand1 = sumTotalExperience(hand1);
+	const experienceHand2 = sumTotalExperience(hand2);
+
+	const isWinner = experienceHand1 > experienceHand2;
+
 	return (
 		<div>
-			<h3>Opponent's Hand</h3>
-			<Pokedex pokemon={hand1} />
-			<h3>Your Hand</h3>
+			<h3>
+				Opponent's Hand
+				<span style={{ fontSize: "0.8em", marginLeft: "10px" }}>
+					(Total Exp: {experienceHand2})
+				</span>
+			</h3>
 			<Pokedex pokemon={hand2} />
+			<h3>
+				Your Hand
+				<span style={{ fontSize: "0.8em", marginLeft: "10px" }}>
+					(Total Exp: {experienceHand1})
+				</span>
+			</h3>
+			<Pokedex pokemon={hand1} />
 		</div>
 	);
 };
