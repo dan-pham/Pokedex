2d25725048d8245c45b28edb194be4cac36b25e6
Dan Pham <phamdan.sj@gmail.com>
2024-10-28 14:59:55 -0700

    Extract data into a separate file

--- src/components/Pokegame.js
+++ src/components/Pokegame.js
@@ -1,9 +1,8 @@
 import React from "react";
 import Pokedex from "./Pokedex";
+import pokemonData from "../data/pokemonData";
 
-const Pokegame = () => {
-	const pokemon = pokemonData;
-
+const Pokegame = ({ pokemon = pokemonData }) => {
 	// Shuffle the pokemon and split into two hands
 	const shuffledPokemon = shuffle(pokemon);
 	const [hand1, hand2] = [shuffledPokemon.slice(0, 4), shuffledPokemon.slice(4)];
@@ -33,16 +32,5 @@ function shuffle(array) {
 
 	return newArray;
 }
-
-const pokemonData = [
-	{ id: 4, name: "Charmander", type: "fire", base_experience: 62 },
-	{ id: 7, name: "Squirtle", type: "water", base_experience: 63 },
-	{ id: 11, name: "Metapod", type: "bug", base_experience: 72 },
-	{ id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
-	{ id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
-	{ id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
-	{ id: 94, name: "Gengar", type: "poison", base_experience: 225 },
-	{ id: 133, name: "Eevee", type: "normal", base_experience: 65 },
-];
 
 export default Pokegame;
